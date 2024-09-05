7f3c96365d356853cb99556378807867d6470b00
tomasmendietarios <tmendieta@itba.edu.ar>
2024-09-05 20:59:10 -0300

    first commit

---
+++ Main/main.py
@@ -0,0 +1,266 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+class StreamlitApp:
+    def __init__(self):
+        self.df_all = pd.DataFrame()  
+        self.setup()  
+        self.initialize_filters() 
+
+    def setup(self):
+        # Load the datasets
+        df_before = pd.read_csv('/Users/tomasmendietarios/Library/Mobile Documents/com~apple~CloudDocs/I.T.B.A/Mi Reloj Interno/Data/allData_MiRelojInterno_27Marzo2023.csv')
+        df_after = pd.read_csv('/Users/tomasmendietarios/Library/Mobile Documents/com~apple~CloudDocs/I.T.B.A/PFC/Mi Reloj Interno/allData_MiRelojInterno_24Julio2024.csv')
+        self.df_all = pd.concat([df_before, df_after], ignore_index=True)
+        
+        # Convert date columns to datetime and sort the data
+        self.df_all['date_recepcion_data'] = pd.to_datetime(self.df_all['date_recepcion_data'])
+        self.df_all.sort_values(by=['user_id', 'date_recepcion_data'], ascending=[True, True], inplace=True)
+        self.df_all.reset_index(drop=True, inplace=True)
+        
+        # Calculate the difference in days between consecutive entries for the same user
+        self.df_all['days_diff'] = self.df_all.groupby('user_id')['date_recepcion_data'].diff().dt.days.fillna(0)
+
+    def initialize_filters(self):
+        
+        if 'date_min' not in st.session_state:
+            st.session_state.date_min = self.df_all['date_recepcion_data'].min()
+        if 'date_max' not in st.session_state:
+            st.session_state.date_max = self.df_all['date_recepcion_data'].max()
+        if 'age' not in st.session_state:
+            st.session_state.age = [self.df_all['age'].min(), self.df_all['age'].max()]
+        if 'selected_gender' not in st.session_state:
+            st.session_state.selected_gender = 'All'
+        if 'df_selected' not in st.session_state:
+            st.session_state.df_selected = self.df_all
+        if 'selected_recomendaciones' not in st.session_state:
+            st.session_state.selected_recomendaciones = 'All'
+        if 'selected_recomendaciones_rango_min' not in st.session_state:
+            st.session_state.selected_recomendaciones_rango_min = 0
+        if 'selected_recomendaciones_rango_max' not in st.session_state:
+            st.session_state.selected_recomendaciones_rango_max = 30
+        if 'categorize_ages' not in st.session_state:
+            st.session_state.categorize_ages = False  # Default to not categorize ages
+
+    def apply_filters(self):
+        # Start with the unfiltered data
+        df_filtered = self.df_all
+
+
+        # Check if session state is initialized and apply filters
+        if not st.session_state.get('all_dates', True):
+            filters_applied = True
+            date_min = pd.to_datetime(st.session_state['date_min'])
+            date_max = pd.to_datetime(st.session_state['date_max'])
+            df_filtered = df_filtered[(df_filtered['date_recepcion_data'] >= date_min) & (df_filtered['date_recepcion_data'] <= date_max)]
+        
+        if not st.session_state.get('all_ages', True):
+            filters_applied = True
+            age_min, age_max = st.session_state['age']
+            df_filtered = df_filtered[(df_filtered['age'] >= age_min) & (df_filtered['age'] <= age_max)]
+        
+        if st.session_state.get('selected_gender', 'All') != 'All':
+            filters_applied = True
+            df_filtered = df_filtered[df_filtered['genero'] == st.session_state['selected_gender']]
+
+        if st.session_state.get('selected_recomendaciones', 'All') != 'All':
+            filters_applied = True
+            days_min = st.session_state['selected_recomendaciones_rango_min']
+            days_max = st.session_state['selected_recomendaciones_rango_max']
+            rec_filter = st.session_state['selected_recomendaciones']
+            
+            if rec_filter == 'Si':
+                df_filtered = df_filtered[(df_filtered['SEGUISTE_RECOMENDACIONES'] == 'si') & 
+                                        (df_filtered['days_diff'] > days_min) & 
+                                        (df_filtered['days_diff'] <= days_max)]
+            elif rec_filter == 'No':
+                df_filtered = df_filtered[(df_filtered['SEGUISTE_RECOMENDACIONES'] == 'no') & 
+                                        (df_filtered['days_diff'] > days_min) & 
+                                        (df_filtered['days_diff'] <= days_max)]
+        
+        # Apply categorization if requested
+        if st.session_state.get('categorize_ages', False):
+            filters_applied = True
+            self.categorize_age(df_filtered)
+        else:
+            # Remove the age category column if it exists
+            if 'age_category' in df_filtered.columns:
+                df_filtered = df_filtered.drop(columns=['age_category'])
+
+ 
+        df_filtered = df_filtered.drop_duplicates(subset='user_id', keep='last')
+
+        # Save the filtered DataFrame back to session state
+        st.session_state['df_selected'] = df_filtered
+
+
+    def display_sidebar(self):
+        st.sidebar.header('Filter Options')
+        
+        # Unique keys for each checkbox
+        st.session_state.all_dates = st.sidebar.checkbox("All Dates", value=True, key='all_dates_checkbox')
+        if not st.session_state.all_dates:
+            st.session_state.date_min = st.sidebar.date_input("Start Date", value=self.df_all['date_recepcion_data'].min(), key='start_date_input')
+            st.session_state.date_max = st.sidebar.date_input("End Date", value=self.df_all['date_recepcion_data'].max(), key='end_date_input')
+        else:
+            st.session_state.date_min = self.df_all['date_recepcion_data'].min()
+            st.session_state.date_max = self.df_all['date_recepcion_data'].max()
+
+        st.session_state.all_ages = st.sidebar.checkbox("All Ages", value=True, key='all_ages_checkbox')
+        if not st.session_state.all_ages:
+            st.session_state.age = st.sidebar.slider(
+                "Age Range", 
+                min_value=int(self.df_all['age'].min()), 
+                max_value=int(self.df_all['age'].max()), 
+                value=(int(self.df_all['age'].min()), int(self.df_all['age'].max())),
+                key='age_range_slider'
+            )
+        else:
+            st.session_state.age = [self.df_all['age'].min(), self.df_all['age'].max()]
+        
+        st.session_state.all_genders = st.sidebar.checkbox("All Genders", value=True, key='all_genders_checkbox')
+        if not st.session_state.all_genders:
+            st.session_state.selected_gender = st.sidebar.selectbox("Select Gender", options=self.df_all['genero'].unique().tolist(), key='gender_selectbox')
+        else:
+            st.session_state.selected_gender = 'All'
+
+        st.session_state.all_recomendaciones = st.sidebar.checkbox("All Recommendations", value=True, key='all_recommendations_checkbox')
+        if not st.session_state.all_recomendaciones:
+            st.session_state.selected_recomendaciones = st.sidebar.selectbox("Seguiste recomendaciones", options=['Si', 'No'], key='recommendations_selectbox')
+            st.session_state.selected_recomendaciones_rango_min = st.sidebar.number_input("Min days difference", min_value=0, max_value=100, value=st.session_state.selected_recomendaciones_rango_min, key='min_days_diff_input')
+            st.session_state.selected_recomendaciones_rango_max = st.sidebar.number_input("Max days difference", min_value=0, max_value=100, value=st.session_state.selected_recomendaciones_rango_max, key='max_days_diff_input')
+        else:
+            st.session_state.selected_recomendaciones = 'All'
+            st.session_state.selected_recomendaciones_rango_min = 0
+            st.session_state.selected_recomendaciones_rango_max = 30
+
+        # Unique key for "Categorize Ages" checkbox
+        st.session_state.categorize_ages = st.sidebar.checkbox("Categorize Ages", value=st.session_state.categorize_ages, key='categorize_ages_checkbox')
+
+        if st.session_state.categorize_ages:
+            st.sidebar.subheader("Define Age Categories")
+            st.session_state.age_joven_min = st.sidebar.slider("Min Age for Jóvenes", min_value=0, max_value=100, value=15, key='age_joven_min_slider')
+            st.session_state.age_adult_min = st.sidebar.slider("Min Age for Adultos", min_value=0, max_value=100, value=30, key='age_adult_min_slider')
+            st.session_state.age_tercera_edad_min = st.sidebar.slider("Min Age for Tercera Edad", min_value=0, max_value=100, value=60, key='age_tercera_edad_min_slider')
+
+        st.session_state.plot_type = st.sidebar.selectbox(
+            "Select Plot Type",
+            options=[
+                "Distribution of Gender", 
+                "Distribution of Recommendations",
+                "Days Difference Histogram",
+                "Exposición a la luz"
+            ],
+            key='plot_type_selectbox'
+        )
+
+        if st.sidebar.button("Apply Filters", key='apply_filters_button'):
+            self.apply_filters()
+
+    def categorize_age(self, df_filtered):
+        # Function to categorize age based on the input age ranges
+        def age_category(age):
+            if age < st.session_state.age_adult_min:
+                return 'Jóvenes'
+            elif age < st.session_state.age_tercera_edad_min:
+                return 'Adultos'
+            else:
+                return 'Tercera Edad'
+
+        df_filtered['age_category'] = df_filtered['age'].apply(age_category)
+
+    def FOTICO_luz_natural_8_15_integrada(self):
+            # Ensure 'age_category' exists in the filtered dataframe
+            if 'age_category' not in st.session_state.df_selected.columns and st.session_state.categorize_ages:
+                self.categorize_age(st.session_state.df_selected)
+
+            # Create an interactive histogram using Plotly with a sub-hue for age category
+            fig = px.histogram(
+                st.session_state.df_selected, 
+                x='FOTICO_luz_natural_8_15_integrada', 
+                color='genero',  # Use color to differentiate between genders
+                barmode='stack',  # Stack the bars to show cumulative counts
+                nbins=3,  # Number of bins
+                facet_col='age_category' if 'age_category' in st.session_state.df_selected.columns else None,  # Add sub-hue for age category if available
+                category_orders={"age_category": ["Jóvenes", "Adultos", "Tercera Edad"],  # Specify the order of age categories
+                            "FOTICO_luz_natural_8_15_integrada": [0, 1, 2]}, 
+                title='Exposición a la luz natural'
+            )
+            fig.for_each_annotation(lambda a: a.update(text=a.text.replace(" FOTICO_luz_natural_8_15_integrada", "")))
+            fig.for_each_annotation(lambda a: a.update(text=a.text.replace(" FOTICO_luz_natural_8_15_integrada ", "")))
+            fig.for_each_annotation(lambda a: a.update(text=a.text.replace("FOTICO_luz_natural_8_15_integrada ", "")))
+            fig.for_each_annotation(lambda a: a.update(text=a.text.replace("FOTICO_luz_natural_8_15_integrada", "")))
+            fig.for_each_annotation(lambda a: a.update(text=a.text.replace("age_category=", "")))
+           
+
+            # Update layout to ensure clear display
+            fig.update_layout(
+                xaxis=dict(tickmode='array', tickvals=[0, 1, 2], ticktext=['0', '1', '2']),
+                yaxis_title='Cantidad de usuarios',
+                legend_title_text='Género'
+            )
+
+            st.plotly_chart(fig)
+    def FOTICO_luz_ambiente_8_15_luzelect_si_no_integrada(self):
+        # Ensure 'age_category' exists in the filtered dataframe
+        if 'age_category' not in st.session_state.df_selected.columns and st.session_state.categorize_ages:
+            self.categorize_age(st.session_state.df_selected)
+
+        # Create an interactive histogram using Plotly with a sub-hue for age category
+        fig = px.histogram(
+            st.session_state.df_selected, 
+            x='FOTICO_luz_ambiente_8_15_luzelect_si_no_integrada', 
+            color='genero',  # Use color to differentiate between genders
+            barmode='stack',  # Stack the bars to show cumulative counts
+            nbins=2,  # Ensure only 0 and 1 are shown on the x-axis
+            facet_col='age_category' if 'age_category' in st.session_state.df_selected.columns else None,  # Add sub-hue for age category if available
+            category_orders={
+                "age_category": ["Jóvenes", "Adultos", "Tercera Edad"],  # Specify the order of age categories
+                "FOTICO_luz_ambiente_8_15_luzelect_si_no_integrada": [0, 1]  # Only show 0 and 1
+            }, 
+            title='Exposición a la luz artificial'
+        )
+
+        # Remove unwanted annotations
+        fig.for_each_annotation(lambda a: a.update(text=a.text.replace("FOTICO_luz_ambiente_8_15_luzelect_si_no_integrada", "")))
+        fig.for_each_annotation(lambda a: a.update(text=a.text.replace("age_category=", "")))
+
+        # Update layout to ensure clear display
+        fig.update_layout(
+            xaxis=dict(tickmode='array', tickvals=[0, 1], ticktext=['0', '1']),  # Only show 0 and 1
+            yaxis_title='Cantidad de usuarios',
+            xaxis_title='',  # This will hide the long variable name on the x-axis
+            legend_title_text='Género'
+        )
+
+        st.plotly_chart(fig)
+
+
+    def display_data_and_plots(self):
+        # Display the filtered data and selected plot
+        filtered_length = len(st.session_state.df_selected)
+        st.write(f'Cantidad de usuarios: {filtered_length}')
+        st.write('Data filtrada', st.session_state.df_selected)
+        
+        if filtered_length > 0:
+            if st.session_state.plot_type ==  "Exposición a la luz":
+                self.FOTICO_luz_natural_8_15_integrada()
+                self.FOTICO_luz_ambiente_8_15_luzelect_si_no_integrada()
+        if filtered_length > 0:
+            if st.session_state.plot_type ==  "Exposición a la luz nartifi":
+                self.FOTICO_luz_natural_8_15_integrada()
+        else:
+            st.write("No data available for the selected filters.")
+    
+    def run(self):
+        self.display_sidebar()
+        self.display_data_and_plots()
+
+if __name__ == "__main__":
+    app = StreamlitApp()
+    app.run()
+
+# To run the app, use the command:
+# streamlit run "/Users/tomasmendietarios/Library/Mobile Documents/com~apple~CloudDocs/I.T.B.A/PFC/Main/main2.py"
